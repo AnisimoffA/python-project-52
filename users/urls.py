98d3b964824c9753084843c75121bbc931ae6c80
Anisimoff <happyprooo@gmail.com>
2023-09-10 13:28:35 +0300

    please work15

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from users.views import * # NOQA F403
 
 urlpatterns = [
     path('', UserList.as_view(), name="users_list"),
-    path('register/', UserRegister.as_view(), name="users_register"),
+    path('create/', UserRegister.as_view(), name="users_register"),
     path('<int:pk>/update/', UserUpdate.as_view(), name="users_update"),
     path('<int:pk>/delete/', UserDelete.as_view(), name="users_delete"),
 ]
