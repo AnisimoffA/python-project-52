210236bfcdcc26f7eafd39c6fc24f8d7e9c75418
Anisimoff <happyprooo@gmail.com>
2023-09-16 13:59:44 +0300

    docker add

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -110,13 +110,23 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # databases for localhost:
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'dm_db',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
